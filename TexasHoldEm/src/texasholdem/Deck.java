1a39339da55c1cbd2fb5dc3a46532062d62c187b
maheshn <nareshkumar1605@gmail.com>
2015-11-11 16:23:36 -0500

    Add javadoc for the Deck module

--- TexasHoldEm/src/texasholdem/Deck.java
+++ TexasHoldEm/src/texasholdem/Deck.java
@@ -3,8 +3,16 @@ package texasholdem;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * Create,store,shuffle and distribute the deck of playing cards
+ * @author Naresh
+ */
+
 public class Deck extends ArrayList<Card> {
 
+/**
+ * The method creates every card(its rank and suit) and adds it to the playing deck
+ */
     public void Set(){
         for(int i = 2; i <= 14; i++){
             Card card = new Card(i,'C');
@@ -23,7 +31,10 @@ public class Deck extends ArrayList<Card> {
             this.add(card);
         }
     }
-    
+   
+/**
+ * The method shuffles the deck of cards
+ */
     public void Shuffle(){
         Collections.shuffle(this);
     }
