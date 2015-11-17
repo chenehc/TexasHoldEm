cdda4667be668de2561e3334576b877727b33020
maheshn <nareshkumar1605@gmail.com>
2015-11-17 17:05:22 -0500

    Add methods to parse the help and rules text files

--- TexasHoldEm/src/texasholdem/ResourceParser.java
+++ TexasHoldEm/src/texasholdem/ResourceParser.java
@@ -1,20 +1,51 @@
 package texasholdem;
 
+import java.io.File;
+import java.io.PrintStream;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ResourceParser {
-	Scanner in = new Scanner(System.in);
+	 //Scanner in = new Scanner(System.in);
+
+	static String readFile(String fileName) throws IOException {
+    BufferedReader br = new BufferedReader(new FileReader(fileName));
+    try {
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
+
+        while (line != null) {
+            sb.append(line);
+            sb.append("\n");
+            line = br.readLine();
+        }
+        return sb.toString();
+    } finally {
+        br.close();
+    }
+}
 	
 	public static String[] parseSave(){
 		String[] temp = new String[2];
 		return temp;
 	}
 	
-	public static String[] parseAbout(){
-		
+	public static String parseHelp() throws IOException{
+		String help = new String();
+		help = readFile("TexasHoldEm/res/Help.txt");
+		return help;
 	}
 	
-	public static String[] parseRule(){
-		
+	public static String parseRule() throws IOException{
+		String rule = new String();
+		rule = readFile("TexasHoldEm/res/Rules.txt");
+		return rule;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		System.out.println(parseHelp());
+		System.out.println(parseRule());
 	}
 }
