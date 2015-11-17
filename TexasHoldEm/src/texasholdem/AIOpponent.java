6d09a4e50c7977928811e4c44abf9ccccceb4174
Sam Hamel <hamels2@mcmaster.ca>
2015-11-17 13:23:18 -0500

    updated AIOpponent

--- TexasHoldEm/src/texasholdem/AIOpponent.java
+++ TexasHoldEm/src/texasholdem/AIOpponent.java
@@ -1,12 +1,74 @@
 package texasholdem;
 
-public class AIOpponent extends Player{
+public class AIOpponent{
 	
-	public AIOpponent(){
-		super();
-	}
-	
-	private void getAction(){
+	public static int bet(Player ai){
+		Hand hand = new Hand();
+		
+		if(hand.getStrength() >=100000000){
+			return ai.getChips();
+			
+		}
+		if(hand.getStrength() >= 10000000){
+			double d = ai.getChips()*0.5;
+			if((int)d == 0){
+				return ai.getChips();
+			}
+			return (int)d;
+		}
+		if(hand.getStrength()>=1000000){
+			double d = ai.getChips()*0.45;
+			if((int)d == 0){
+				return ai.getChips();
+			}
+			return (int)d;
+		}
+		if(hand.getStrength()>=100000){
+			double d = ai.getChips()*0.4;
+			if((int)d == 0){
+				return ai.getChips();
+			}
+			return (int)d;
+		}
+		if(hand.getStrength()>=10000){
+			double d = ai.getChips()*0.35;
+			if((int)d == 0){
+				return ai.getChips();
+			}
+			return (int)d;
+		}
+		if(hand.getStrength()>=10000){
+			double d = ai.getChips()*0.3;
+			if((int)d == 0){
+				return ai.getChips();
+			}
+			return (int)d;
+		}
+		if(hand.getStrength()>=1000){
+			double d = ai.getChips()*0.2;
+			if((int)d == 0){
+				return ai.getChips();
+			}
+			return (int)d;
+		}
+		if(hand.getStrength()>=100){
+			double d = ai.getChips()*0.1;
+			if((int)d == 0){
+				return ai.getChips();
+			}
+			return (int)d;
+		}
+		if(hand.getStrength()>=10){
+			double d = ai.getChips()*0.05;
+			if((int)d == 0){
+				return ai.getChips();
+			}
+			return (int)d;
+		}
+		if(hand.size()>5&&hand.getStrength()<15){
+			return 0;
+		}
+		return hand.getStrength();
 		
 	}
 }
